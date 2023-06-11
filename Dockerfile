0fdc59bef5505d438bc3916ca1f4d768224a0342
Alham <alhammanazil18@gmail.com>
2023-06-11 20:09:09 +0700

    edit some requirements

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
-FROM python:3.10.3-slim-buster
+FROM python:3.9
+
+COPY ./requirements.txt /app/requirements.txt
+
+RUN pip install --no-cache-dir -r /app/requirements.txt
+
+COPY . /app
 
 WORKDIR /app
 
-COPY requirements.txt requirements.txt
-
-RUN pip install -r requirements.txt
-
-COPY . .
-
-ENV PYTHONUNBUFFERED=1
-
-ENV HOST 0.0.0.0
-
-EXPOSE 8080
-
-CMD ["python", "main.py"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8081"]
--- main.py
+++ main.py
@@ -1,41 +1,30 @@
-import os
-import uvicorn
 import traceback
-import tensorflow.compat.v2 as tf
+import tensorflow as tf
+from pydantic import BaseModel
 import numpy as np
 from typing import List
-tf.enable_v2_behavior()
-import h5py
 
-from pydantic import BaseModel
 from urllib.request import Request
 from fastapi import FastAPI, Response, UploadFile
 from utils import load_image_into_numpy_array
 
 app = FastAPI()
 
-# This endpoint is for a test (or health check) to this server
 @app.get("/")
 def index():
     return "Hello world from ML endpoint!"
 
-# If your model needs text input, use this endpoint!
 class RequestText(BaseModel):
     text: str
 
-# here we use tflite for some reason
 interpreter = tf.lite.Interpreter(model_path="converted_model.tflite")
 def recommendtflite(textinput):
     interpreter.allocate_tensors()
 
-    # get input and output tensors
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    # print(input_details)
-
     interpreter.set_tensor(input_details[0]['index'], np.array([textinput]))
-    # interpreter.set_tensor(input_details[0]['index'], np.array(["Pfizer"]))
 
     interpreter.invoke()
 
@@ -60,7 +49,6 @@ def recommendrawmodel(textinputarr):
 @app.post("/predict_text")
 def predict_text(req: RequestText, response: Response):
     try:
-        # In here you will get text sent by the user
         text = req.text
         print("Uploaded text:", text)
         result = recommendtflite(text)
@@ -82,10 +70,3 @@ def predict_text(req: List[str], response: Response):
         traceback.print_exc()
         response.status_code = 500
         return "Internal Server Error"
-
-
-# Starting the server
-# You can check the API documentation easily using /docs after the server is running
-# port = os.environ.get("PORT", 8080)
-# print(f"Listening to http://0.0.0.0:{port}")
-# uvicorn.run(app, host='0.0.0.0', port=port)
